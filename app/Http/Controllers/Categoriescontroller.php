9202935777a3fcc2cee6115af11b86a456760aff
NAO5802 <i100185b@gmail.com>
2020-01-01 15:26:48 +0900

    initial commit

---
+++ app/Http/Controllers/Categoriescontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class Categoriescontroller extends Controller
+{
+    //
+    public function index(){
+        $names = Category::all();
+        return view('categories.index', ['names' => $names]);
+    }
+}
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Blog</title>
+  <link href="/css/style.css" rel="stylesheet" type="text/css">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="js/style.js"></script>
+</head>
+<body>
+  <div class="container">
+    <h1>Blog</h1>
+    <ul>
+      <!-- @foreach ($names as $name)
+      <li><a href="">{{ $name->name }}</a></li>
+      @endforeach -->
+
+      @forelse ($names as $name)
+        <li><a href="">{{ $name->name }}</a></li>
+      @empty
+        <li>no data</li>
+      @endforelse
+    </ul>
+  </div>
+</body>
