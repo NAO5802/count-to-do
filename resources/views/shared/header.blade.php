f563b05a90827febdaa726a2e781e0c796734cd1
NAO5802 <i100185b@gmail.com>
2020-01-09 12:23:59 +0900

    markup header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,28 +1,33 @@
-<div class="header">
+<div class="header shadow-sm px-3 bg-skyblueD d-flex justify-content-between">
   <div class="header-left">
-    <a href="{{ url('/') }}">Count ToDo</a>
+    <a href="{{ url('/') }}" class="text-decoration-none h5 header-link">Count ToDo</a>
   </div>
   <div class="header-right">
-    <div class="header-right__menu">
-      <ul>
+    <div class="header-right__menu  d-inline-flex">
         @guest
-          <li><a href="{{ route('login') }}">ログイン</a></li>
-          <li><a href="{{ route('register') }}">新規登録</a></li>
+          <div><a class="header-link text-decoration-none mr-3" href="{{ route('login') }}">ログイン</a></div>
+          <div><a class="header-link text-decoration-none " href="{{ route('register') }}">新規登録</a></div>
         @else
-          <li><a href="{{ action( 'TasksController@create' ) }}">追加</a></li>
-          <li><a href="{{ action( 'UsersController@show', $user ) }}">Task集計</a></li>
-          <li>
-            <a href="{{ route('logout') }}"
+          <div><a href="{{ action( 'TasksController@create' ) }}"><i class="fas fa-plus h4 mr-4 header-link"></i></a></div>
+          <div class="dropdown">
+            <a class="btn dropdown-toggle p-0" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-bars h4 header-link mb-2"></i>
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="{{ action( 'UsersController@show', $user ) }}">Task集計</a>
+              <a class="dropdown-item" href="{{ action( 'UsersController@edit', $user ) }}">ユーザー設定</a>
+              <a class="dropdown-item" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                               document.getElementById('logout-form').submit();">
                 {{ __('ログアウト') }}
             </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+            </div>
+          </div>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-          </li>
-          <li><a href="{{ action( 'UsersController@edit', $user ) }}">ユーザー設定</a></li>
         @endguest
       </ul>
     </div>
